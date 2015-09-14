d1b8a43d57e7448880fe164e932cbb6dd5635d97
Silvana Trindade <syletri@gmail.com>
2015-09-13 21:36:29 -0300

    Analisando frequência de caractere

--- trabalho4/construct_dictionary.py
+++ trabalho4/construct_dictionary.py
@@ -1,4 +1,5 @@
 from sys import argv
+import operator
 
 class ConstructDictionary():
 	def construct(self,file):
@@ -29,3 +30,93 @@ class ConstructDictionary():
 				f.write(i+"\n")				
 
 		return dic;
+
+	def constructMonographs(self,file):
+		
+		dictionary = {}
+
+		with open(file, "r+b") as f:
+			while 1:
+			 	read_data = f.read(1)
+
+			 	if not read_data:
+			 		break
+			 	elif " " == read_data or "\r" == read_data or "\t" == read_data or "\n" == read_data:
+			 		continue
+			 	else:
+			 		if read_data in dictionary :	
+			 			dictionary[read_data] += 1
+			 		else:
+			 			dictionary[read_data] = 1
+
+		monographs = "monographs.txt"
+		
+		with open(monographs, "w+b") as f:
+			for one in sorted(dictionary,key=dictionary.get,reverse=True):
+				monograph = one+" "+str(dictionary[one])+"\n"
+				f.write(monograph)
+
+	def constructDigraphs(self,file):
+		
+		dictionary = {}
+
+		s = ""
+
+		with open(file, "r+b") as f:
+			while 1:
+			 	read_data = f.read(1)
+
+			 	if not read_data:
+			 		break
+			 	elif not s:
+			 		s = read_data
+			 	elif " " == read_data or "\r" == read_data or "\t" == read_data or "\n" == read_data:
+			 		continue
+			 	elif s:
+			 		if s+read_data in dictionary :	
+			 			dictionary[s+read_data] += 1
+			 		else:
+			 			dictionary[s+read_data] = 1
+			 		s = read_data
+
+		digraphs = "digraphs.txt"
+		
+		with open(digraphs, "w+b") as f:
+			for pair in sorted(dictionary,key=dictionary.get,reverse=True):
+				digraph = pair+" "+str(dictionary[pair])+"\n"
+				f.write(digraph)
+
+	def constructTrigraphs(self,file):
+		
+		dictionary = {}
+
+		s = ""
+		t = ""
+
+		with open(file, "r+b") as f:
+			while 1:
+			 	read_data = f.read(1)
+
+			 	if not read_data:
+			 		break
+			 	elif " " == read_data or "\r" == read_data or "\t" == read_data or "\n" == read_data:
+			 		continue
+			 	elif not s:
+			 		s = read_data
+			 	elif not t:
+			 		t = s
+			 		s = read_data
+			 	else:
+			 		if t+s+read_data in dictionary :	
+			 			dictionary[t+s+read_data] += 1
+			 		else:
+			 			dictionary[t+s+read_data] = 1
+			 		t = s
+			 		s = read_data
+
+		trigraphs = "trigraphs.txt"
+		
+		with open(trigraphs, "w+b") as f:
+			for triplet in sorted(dictionary,key=dictionary.get,reverse=True):
+				trigraph = triplet+" "+str(dictionary[triplet])+"\n"
+				f.write(trigraph)
--- trabalho4/main.py
+++ trabalho4/main.py
@@ -2,14 +2,14 @@ from sys import argv
 from substitution_break import SubstitutionBreak
 from construct_dictionary import ConstructDictionary 
 
-script,enc = argv
+#script,enc = argv
 
 dic = ConstructDictionary();
 
-print dic.construct("words.txt");
+# print dic.constructDigraphs("dictionary.txt");
+print dic.constructMonographs("dictionary.txt");
+#substitution = SubstitutionBreak();
 
-substitution = SubstitutionBreak();
-
-substitution.beakOpen(enc);
+#substitution.beakOpen(enc);
 
 
